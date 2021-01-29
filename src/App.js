f48c9cab54b2f9cd2ab1c044af29a11081f3ffe9
Luiz Mariz <lfsmariz@gmail.com>
2021-01-29 11:45:50 -0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/Home";
 import { Provider } from "react-redux";
-import store from './redux/store'
+import store from "./redux/store";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import FormPage from "./pages/FormPage";
 
 function App() {
   return (
-    <Provider store={store}>
-      <div>
-        <Home />
-      </div>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <Switch>
+          <Route exact path={'/'} component={Home}/>
+          <Route exact path={'/form'} component={FormPage}/>
+        </Switch>
+      </Provider>
+    </BrowserRouter>
   );
 }
 
