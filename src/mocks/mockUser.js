197648ff84cd71b074ca90cb964656174c672dd9
Luiz Mariz <lfsmariz@gmail.com>
2021-01-29 11:11:56 -0300

    mock initial state

---
+++ src/mocks/mockUser.js
@@ -0,0 +1,20 @@
+const mockUser = [
+  {
+    id: 1,
+    name: 'Joao',
+    email: 'aaaa@aaa.aa',
+    password: '111111',
+    birthdate: '11/01/2000',
+    userType: 'gerente',
+  },
+  {
+    id: 2,
+    name: 'maria',
+    email: 'bbbb@bbb.bb',
+    password: '222222',
+    birthdate: '12/01/2000',
+    userType: 'colaborador',
+  }
+];
+
+export default mockUser
